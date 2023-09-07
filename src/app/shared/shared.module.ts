5576ce6917534958def05bd9ec66891d4fbda1fa
l.vinciullo <l.vinciullo@eataly.it>
2023-09-07 18:36:55 +0200

    [Shared] refactor base container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,25 +2,28 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
-import { HeaderComponent } from './components/header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { BaseFeatureContainerComponent } from 'fab-shared/components/base-feature-container/base-feature-container.component';
+import { HeaderComponent } from 'fab-shared/components/header/header.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule,
     TranslateModule,
     MatIconModule,
   ],
   declarations: [
     HeaderComponent,
+    BaseFeatureContainerComponent,
   ],
   exports: [
     CommonModule,
     TranslateModule,
     MatButtonModule,
     MatIconModule,
-    HeaderComponent,
+    BaseFeatureContainerComponent,
   ],
 })
 export class SharedModule {
