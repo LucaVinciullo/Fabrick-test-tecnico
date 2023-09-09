e3fc3775f7f726883ef562a5f3bd3c62a5a632aa
l.vinciullo <l.vinciullo@eataly.it>
2023-09-09 17:14:06 +0200

    [Inquiry] Form Validation and api

--- src/app/feature-modules/inquiry/containers/inquiry-container/inquiry-container.component.ts
+++ src/app/feature-modules/inquiry/containers/inquiry-container/inquiry-container.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InquiryFacadeService } from 'fab-features/inquiry/services/inquiry.facade.service';
+import { AbstractSmartContainerClass } from 'fab-shared/components/abstract/abstract-smart-container.class';
+import { ControlsOf } from 'fab-shared/model/controls-of.type';
 
 @Component({
   selector: 'fab-inquiry-container',
   templateUrl: './inquiry-container.component.html',
   styleUrls: ['./inquiry-container.component.scss'],
 })
-export class InquiryContainerComponent {
+export class InquiryContainerComponent extends AbstractSmartContainerClass {
+  shouldClearStateOnDestroy = false;
 
+  users$ = this.facade.users$;
+
+  idForm: FormGroup<ControlsOf<{ id: number | null }>> = new FormGroup({
+    id: new FormControl<number | null>(null, [Validators.required, Validators.pattern('^[0-9]*$')]),
+  });
+
+  get hasPatternError(): boolean {
+    return !!this.idForm.controls.id.errors?.['pattern'];
+  }
+
+  constructor(override facade: InquiryFacadeService) {
+    super(facade);
+    this.subscription.add(
+      this.idForm.controls.id.valueChanges.subscribe(() => {
+        if (this.hasPatternError) this.idForm.controls.id.markAsTouched();
+      }),
+    );
+  }
+
+  inquiryUser() {
+    const { value } = this.idForm.controls.id;
+    if (value) this.facade.userInquiry(value);
+  }
 }
--- src/app/feature-modules/inquiry/inquiry.module.ts
+++ src/app/feature-modules/inquiry/inquiry.module.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatOptionModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { InquiryContainerComponent } from 'fab-features/inquiry/containers/inquiry-container/inquiry-container.component';
 import { InquiryRoutingModule } from 'fab-features/inquiry/inquiry-routing.module';
 import { InquiryFacadeService } from 'fab-features/inquiry/services/inquiry.facade.service';
@@ -19,10 +17,10 @@ import { SharedModule } from 'fab-shared/shared.module';
     SharedModule,
     InquiryRoutingModule,
     MatFormFieldModule,
-    MatInputModule,
-    MatOptionModule,
-    MatSelectModule,
+    FormsModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [
     InquiryFacadeService,
