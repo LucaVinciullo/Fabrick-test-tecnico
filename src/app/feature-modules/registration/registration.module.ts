3c06370a8c785148e6567f5d3d443ff84e9cf1c1
l.vinciullo <l.vinciullo@eataly.it>
2023-09-10 23:05:55 +0200

    [Registration] enhance tooltip visibility for gender select and registration feedback

--- src/app/feature-modules/registration/registration.module.ts
+++ src/app/feature-modules/registration/registration.module.ts
@@ -1,7 +1,9 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -26,6 +28,8 @@ import { SharedModule } from 'fab-shared/shared.module';
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
+    MatIconModule,
+    ClipboardModule,
   ],
   providers: [
     RegistrationFacadeService,
--- src/app/feature-modules/registration/services/registration.facade.service.ts
+++ src/app/feature-modules/registration/services/registration.facade.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { User } from 'fab-core/api/model/user.interface';
 import { UserApiService } from 'fab-core/api/services/user.api.service';
-import { NotificationService } from 'fab-core/notification/services/notification.service';
 import { UserFormValue } from 'fab-features/registration/model/user-form-value.interface';
 import { AbstractSmartFacadeClass } from 'fab-shared/components/services/abstract-smart-facade.class';
 import { BehaviorSubject } from 'rxjs';
@@ -14,16 +14,21 @@ export class RegistrationFacadeService extends AbstractSmartFacadeClass {
 
   userForm$ = this.userFormSubject$.asObservable();
 
-  constructor(private userApiService: UserApiService, private notificationService: NotificationService) {
+  lastRegisteredUserSubj$ = new BehaviorSubject<User | null>(null);
+
+  lastRegisteredUser$ = this.lastRegisteredUserSubj$.asObservable();
+
+  constructor(private userApiService: UserApiService) {
     super();
   }
 
   userRegistration(userFormValue: NonNullable<UserFormValue>) {
+    this.lastRegisteredUserSubj$.next(null);
     this.subscription?.add(this.userApiService.registerUser(userFormValue).pipe(
       take(1),
       map((user) => {
-        this.notificationService.info('registration.complete', { userId: user.id });
         this.userFormSubject$.next(null);
+        this.lastRegisteredUserSubj$.next(user);
       }),
     ).subscribe());
   }
@@ -33,6 +38,7 @@ export class RegistrationFacadeService extends AbstractSmartFacadeClass {
   }
 
   clearObservables() {
+    this.lastRegisteredUserSubj$.next(null);
     this.userFormSubject$.next(null);
   }
 }
